ad4e7779a97eb1a846d85652a5cd48a356ce00e8
gbrueggman <gab75@pitt.edu>
2020-07-18 11:48:35 -0400

    update pin config

--- Arduino/OmniControlTest/configuration.h
+++ Arduino/OmniControlTest/configuration.h
@@ -1,24 +1,24 @@
 /*Declare L298N Dual H-Bridge Motor Controller directly since there is not a library to load.*/
 //Define L298N Dual H-Bridge Motor Controller Pins
-#define dir1PinL  2    //Motor direction
-#define dir2PinL  4    //Motor direction
-#define speedPinL 6    // Needs to be a PWM pin to be able to control motor speed
+#define dir1PinL  10    //Motor direction
+#define dir2PinL  9    //Motor direction
+#define speedPinL 2    // Needs to be a PWM pin to be able to control motor speed
 
-#define dir1PinR  7    //Motor direction
-#define dir2PinR  8   //Motor direction
-#define speedPinR 5    // Needs to be a PWM pin to be able to control motor speed
+#define dir1PinR  3    //Motor direction
+#define dir2PinR  11   //Motor direction
+#define speedPinR 4    // Needs to be a PWM pin to be able to control motor speed
 
-#define Echo_PIN_FL    11 // Ultrasonic Echo pin connect to D11
-#define Trig_PIN_FL    12  // Ultrasonic Trig pin connect to D12
+#define Echo_PIN_FL    A4 // Ultrasonic Echo pin connect to D11
+#define Trig_PIN_FL    8  // Ultrasonic Trig pin connect to D12
 
-#define Echo_PIN_FR    11 // Ultrasonic Echo pin connect to D11
-#define Trig_PIN_FR    12  // Ultrasonic Trig pin connect to D12
+#define Echo_PIN_FR    A3 // Ultrasonic Echo pin connect to D11
+#define Trig_PIN_FR    7  // Ultrasonic Trig pin connect to D12
 
-#define Echo_PIN_L    11 // Ultrasonic Echo pin connect to D11
+#define Echo_PIN_L    A4 // Ultrasonic Echo pin connect to D11
 #define Trig_PIN_L    12  // Ultrasonic Trig pin connect to D12
 
-#define Echo_PIN_R    11 // Ultrasonic Echo pin connect to D11
-#define Trig_PIN_R    12  // Ultrasonic Trig pin connect to D12
+#define Echo_PIN_R    A5 // Ultrasonic Echo pin connect to D11
+#define Trig_PIN_R    13  // Ultrasonic Trig pin connect to D12
 
 
 #define SPEED  200     //both sides of the motor speed
